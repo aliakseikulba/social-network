9fe89960e595bcd218c98fa19a17ee04b15d167e
aliakseikulba <kulba.av@gmail.com>
2021-10-13 13:28:16 +0300

    create Profile component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 const App = () => {
@@ -9,22 +10,7 @@ const App = () => {
     <div className='app-wrapper'>
       <Header/>
       <Navbar/>
-      <div className='content'>
-        <div>
-          <img src="https://bernardmarr.com/img/30%20Real%20Examples%20Of%20Blockchain%20Technology%20In%20Practice.png" alt="content logo"/>
-        </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          my posts
-          <div>new post</div>
-        </div>
-        <div>
-          <div>Post1</div>
-          <div>Post2</div>
-        </div>
-      </div>
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://bernardmarr.com/img/30%20Real%20Examples%20Of%20Blockchain%20Technology%20In%20Practice.png" alt="content logo"/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        my posts
+        <div>new post</div>
+      </div>
+      <div>
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;

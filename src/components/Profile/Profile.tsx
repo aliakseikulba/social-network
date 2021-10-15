19babb2b84fde9c31bb398e4ce449d958654c3d1
aliakseikulba <kulba.av@gmail.com>
2021-10-15 15:26:34 +0300

    add content wrapper in App component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
-    <div className={styles.content}>
+    <div>
       <div>
         <img src="https://bernardmarr.com/img/30%20Real%20Examples%20Of%20Blockchain%20Technology%20In%20Practice.png"
              alt="content logo"/>

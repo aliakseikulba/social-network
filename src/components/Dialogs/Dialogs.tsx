4ff2ca64ab9b065cd596f8cb7e6d6c7edf72bb52
aliakseikulba <kulba.av@gmail.com>
2021-10-18 16:20:58 +0300

    lesson 24 completed (data arrays)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -29,19 +29,32 @@ const Message = (props: MessageType) => {
 }
 
 const Dialogs = () => {
+
+  let dialogsData = [
+    {id: 1, name: 'User One'},
+    {id: 2, name: 'User Two'},
+    {id: 3, name: 'User Three'},
+    {id: 4, name: 'User Four'},
+    {id: 5, name: 'User Five'},
+    {id: 6, name: 'User Six'}
+  ]
+
+  let messageData = [
+    {id: 1, message: 'Hello, how it\'s going?'},
+    {id: 2, message: 'Tomorrow at 9.'},
+    {id: 3, message: 'Have a nice day!'}
+  ]
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <Dialog id={1} name='User 1'/>
-        <Dialog id={2} name='User 2'/>
-        <Dialog id={3} name='User 3'/>
-        <Dialog id={4} name='User 4'/>
-        <Dialog id={5} name='User 5'/>
+        <Dialog id={dialogsData[0].id} name={dialogsData[0].name}/>
+        <Dialog id={dialogsData[1].id} name={dialogsData[1].name}/>
       </div>
       <div className={s.messages}>
-        <Message message='Hello, how are you?'/>
-        <Message message='Tomorrow at 9.'/>
-        <Message message='Have a nice day!'/>
+        <Message message={messageData[0].message}/>
+        <Message message={messageData[1].message}/>
+        <Message message={messageData[2].message}/>
       </div>
     </div>
   );

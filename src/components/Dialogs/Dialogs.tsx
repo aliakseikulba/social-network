ce42c4088d2139a3351c70026be6c0de040cd831
aliakseikulba <kulba.av@gmail.com>
2021-10-21 14:50:41 +0300

    dialogs map render

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import s from './Dialogs.module.scss';
 
 type DialogPropsType = {
@@ -9,14 +9,14 @@ type DialogPropsType = {
 
 const Dialog = (props: DialogPropsType) => {
 
-  let path = `/dialogs/${props.id}`
+  let path = `/dialogs/${props.id}`;
 
   return (
     <div className={s.dialog}>
       <NavLink to={path}>{props.name}</NavLink>
     </div>
-  )
-}
+  );
+};
 
 type MessageType = {
   message: string
@@ -25,36 +25,36 @@ type MessageType = {
 const Message = (props: MessageType) => {
   return (
     <div className={s.message}>{props.message}</div>
-  )
-}
+  );
+};
 
 const Dialogs = () => {
 
-  let dialogsData = [
+  const dialogsData = [
     {id: 1, name: 'User One'},
     {id: 2, name: 'User Two'},
     {id: 3, name: 'User Three'},
     {id: 4, name: 'User Four'},
     {id: 5, name: 'User Five'},
     {id: 6, name: 'User Six'}
-  ]
+  ];
 
-  let messageData = [
+  const messageData = [
     {id: 1, message: 'Hello, how it\'s going?'},
     {id: 2, message: 'Tomorrow at 9.'},
     {id: 3, message: 'Have a nice day!'}
-  ]
+  ];
+
+  const dialogsItem = dialogsData.map(d => <Dialog id={d.id} name={d.name}/>);
+  const messagesItem = messageData.map(m => <Message message={m.message}/>);
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <Dialog id={dialogsData[0].id} name={dialogsData[0].name}/>
-        <Dialog id={dialogsData[1].id} name={dialogsData[1].name}/>
+        {dialogsItem}
       </div>
       <div className={s.messages}>
-        <Message message={messageData[0].message}/>
-        <Message message={messageData[1].message}/>
-        <Message message={messageData[2].message}/>
+        {messagesItem}
       </div>
     </div>
   );

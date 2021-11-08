dd1b2f0066b4b4c9643fd035c10876486ee9d12b
aliakseikulba <kulba.av@gmail.com>
2021-11-08 15:07:00 +0300

    lesson 37 completed

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import {store} from './redux/state';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import './index.css';
 
 let rerenderEntireTree = () => {
   ReactDOM.render(
-    <React.StrictMode>
       <App state={store.getState()}
            addPost={store.addPost.bind(store)}
-           updateNewPostText={store.updateNewPostText.bind(store)}/>
-    </React.StrictMode>,
+           updateNewPostText={store.updateNewPostText.bind(store)}/>,
     document.getElementById('root')
   );
 };

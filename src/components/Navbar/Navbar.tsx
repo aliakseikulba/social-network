692f71e43523cd85b6167e7b0f6c64016c39f36d
aliakseikulba <kulba.av@gmail.com>
2021-12-10 18:19:57 +0300

    add Navlink to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,6 +18,9 @@ const Navbar: React.FC = () => {
           <NavLink to="/news" activeClassName={styles.activeLink}>News</NavLink>
         </div>
         <div className={styles.item}>
+          <NavLink to="/users" activeClassName={styles.activeLink}>Users</NavLink>
+        </div>
+        <div className={styles.item}>
           <NavLink to="/music" activeClassName={styles.activeLink}>Music</NavLink>
         </div>
         <div className={styles.item}>
